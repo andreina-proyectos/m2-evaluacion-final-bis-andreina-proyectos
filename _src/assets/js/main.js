8b4e4defc6076c9a62a6b33fe24092fb41d8e8b8
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-31 13:20:36 +0200

    Add function to create new li element for each card

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,6 +4,8 @@ console.log('>> Ready :)');
 //variables
 const startButton = document.querySelector('.start-btn');
 const allInputRadios = document.querySelectorAll('.form__input-radio');
+const gameCardsList = document.querySelector('.game__cards-list');
+
 
 //functions
 
@@ -30,14 +32,40 @@ function handleStartButton () {
 
       for (let i=0; i<data.length; i++) {
         resultImageUrl = data[i].image;
-        resultPokemonName = data[i].image;
-        createElementCards ();
+        resultPokemonName = data[i].name;
+
+        createElementCards (resultImageUrl, resultPokemonName);
+
       }
     });
 }
 
 
-function createElementCards () {}
+function createElementCards (frontCardSrc, frontCardName) {
+
+  //create li
+  const cardNewElement = document.createElement('li');
+  cardNewElement.classList.add('game__card-element');
+
+  //create pokemon front img
+  const imgFrontCard = document.createElement('img');
+  imgFrontCard.classList.add('card__front-img');
+  imgFrontCard.src = frontCardSrc;
+  imgFrontCard.alt = frontCardName;
+
+  //create pokemon back img
+  const imgBackCard = document.createElement('img');
+  imgBackCard.classList.add('card__back-img');
+  imgBackCard.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+  imgBackCard.alt = frontCardName;
+
+  //integrate imgs on li
+  cardNewElement.appendChild(imgFrontCard);
+  cardNewElement.appendChild(imgBackCard);
+
+  //integrate li on ul
+  gameCardsList.appendChild(cardNewElement);
+}
 
 //listeners
 
